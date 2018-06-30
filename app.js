6174c708ba6f5eaade345c16c2f6f2425b342937
sabah khan <sabahkhan17@gmail.com>
2018-06-30 12:39:25 -0400

    update

--- app.js
+++ app.js
@@ -79,7 +79,7 @@ function nextPlayer() {
     document.querySelector('.dice').style.display = 'none';
 }
 
-Document.querySelector('btn-new').addEventListener('click', init);
+document.querySelector('.btn-new').addEventListener('click', init);
 
 function init() {
 
@@ -93,4 +93,9 @@ function init() {
     document.getElementById('score-1').textContent = '0';
     document.getElementById('current-0').textContent = '0';
     document.getElementById('current-1').textContent = '0';
+    document.getElementById('name-0').textContent = 'Player 1';
+    document.getElementById('name-1').textContent = 'Player 2';
+    document.querySelector('.player-0-panel').classList.remove('winner');
+    document.querySelector('.player-1-panel').classList.remove('winner');
+
 }
